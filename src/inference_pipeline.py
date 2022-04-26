a0f5cdf2f76744b3f6efdb198a16d8c31ba10d69
ivam98 <ivam98@mi.fu-berlin.de>
2022-04-26 22:57:01 +0200

    added single, batch and no-bees inference, added visualization

--- src/inference_pipeline.py
+++ src/inference_pipeline.py
@@ -1,4 +1,6 @@
+from optparse import Option
 import os
+from pathlib import Path
 from statistics import mode
 from typing import List, Optional, Union
 
@@ -13,12 +15,18 @@ from torch.utils.data import DataLoader
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
 
-from src.dataset.dataset import CustomDataset
-from src.model.segmentation_model import HoneyBeeCombSegmentationModel
-from src.utils.utils import read_config
+
+import matplotlib.pyplot as plt
+
+
+from src.dataset.CustomDataset import CustomDataset
+from src.model.HoneyBeeCombSegmentationModel import HoneyBeeCombSegmentationModel
+from src.utils.utils import read_config, get_cmap_and_labels_for_plotting
 
 project_path = Path(__file__).parent.parent.parent
 config_path_default = os.path.join(project_path, "config", "config.yaml")
+label_classes_path_default = os.path.join(project_path, "data", "label_classes.json")
+output_folder_for_masks_default = os.path.join(project_path, "data", "inferred_masks")
 
 
 class HoneyBeeCombInferer:
@@ -27,15 +35,19 @@ class HoneyBeeCombInferer:
         path_to_images: str,
         model_name: str,
         config_path: str = config_path_default,
+        label_classes_path: str = label_classes_path_default,
         sw_inference: bool = True,
         device: str = "cpu",
+        output_folder_for_masks: str = output_folder_for_masks_default,
     ):
 
         self.path_to_images = path_to_images
         self.device = device
         self.sw_inferer = sw_inference
+        self.output_folder_for_masks = output_folder_for_masks
 
         self.config = read_config(config_path)
+        self.cmap, self.patches = get_cmap_and_labels_for_plotting(label_classes_path)
 
         self.model = HoneyBeeCombSegmentationModel(model_name=model_name, device=device)
 
@@ -67,7 +79,9 @@ class HoneyBeeCombInferer:
         for image, image_path in tqdm(dataloader):
             inferred_mask = self.infer(image)
 
-            visualizer.save_pred(inferred_mask)
+            self._save_batch_inference(inferred_mask, image_path)
+
+        return None
 
     def infer_without_bees(self, images_path: str) -> None:
 
@@ -96,9 +110,13 @@ class HoneyBeeCombInferer:
 
         output_means.append(output.mean(dim=0))
 
+        pred_logits_no_bees = torch.stack(output_means).mean(dim=0)
+
+        return self._get_mask(pred_logits_no_bees)
+
     def _get_mask(self, inferred_logits: Tensor) -> Tensor:
 
-        return torch.argmax(inferred_logits.squeeze(), dim=0)
+        return torch.argmax(inferred_logits.squeeze(), dim=0).detach().cpu().numpy()
 
     def preprocess_raw_image(self, image: Union[Tensor, np.array]) -> Tensor:
 
@@ -132,3 +150,64 @@ class HoneyBeeCombInferer:
         inferred_logits_pred = torch.softmax(inferred_logits_pred, dim=0).clone()
 
         return torch.argmax(inferred_logits_pred, dim=0)
+
+    def plot_prediction(
+        self,
+        pred: Tensor,
+        input_image: Optional[np.ndarray] = None,
+        mask: Optional[np.ndarray] = None,
+    ) -> None:
+
+        label_processed = np.array([[self.cmap[int(i)] for i in j] for j in tqdm(pred)])
+
+        if input_image is not None and mask is not None:
+            fig, ax = plt.subplots(3, 1, figsize=(40, 35))
+            ax[0].imshow(input_image)
+            ax[0].set_title("input image")
+
+            ax[1].imshow(label_processed)
+            ax[1].set_title("predicted")
+
+            mask_processed = np.array([[self.cmap[i] for i in j] for j in tqdm(mask)])
+            ax[2].imshow(mask_processed)
+            ax[2].set_title("ground truth")
+
+        elif input_image is None or mask is not None:
+            fig, ax = plt.subplots(2, 1, figsize=(40, 35))
+
+            if input_image is not None:
+                ax[0].imshow(input_image)
+                ax[0].set_title("input image")
+            elif mask is not None:
+                mask_processed = np.array([[self.cmap[i] for i in j] for j in tqdm(mask)])
+                ax[0].imshow(input_image)
+                ax[0].set_title("input image")
+
+            ax[1].imshow(label_processed)
+            ax[1].set_title("predicted")
+
+        else:
+            fig, ax = plt.subplots(1, 1, figsize=(24, 20))
+
+            ax.imshow(label_processed)
+            ax.set_title("predicted")
+
+        plt.legend(handles=self.patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
+        plt.show()
+
+        return None
+
+    def _save_batch_inference(self, pred: Tensor, input_image_path: str) -> None:
+
+        output_path = input_image_path.replace("images", "inferred_masks")
+
+        label_processed = np.array([[self.cmap[int(i)] for i in j] for j in tqdm(pred)])
+        fig, ax = plt.subplots(1, 1, figsize=(24, 20))
+
+        ax.imshow(label_processed)
+        ax.set_title("predicted")
+        plt.legend(handles=self.patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
+        plt.savefig(output_path)
+        plt.close(fig)
+
+        return None
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,7 +1,13 @@
 import os
+from pathlib import Path
 import yaml
+import json
+import numpy as np
 
-from typing import List
+from PIL import ImageColor
+import matplotlib.patches as mpatches
+
+from typing import List, Tuple
 import torch
 
 
@@ -18,3 +24,34 @@ def read_config(config_path: str) -> dict:
     config = yaml.load(stream)
 
     return config
+
+
+def read_label_classes(label_classes_path: str) -> dict:
+
+    with open(label_classes_path, "r") as f:
+        label_classes = json.load(f)
+    return label_classes
+
+
+def get_cmap_and_labels_for_plotting(label_classes_path: str) -> Tuple[dict, List[mpatches.Patch]]:
+
+    # read label classes config (extracted from hasty.ai after labeling)
+    label_classes = read_label_classes(label_classes_path)
+
+    # prepare labels and colors
+    cmap = {}
+    labels = {}
+
+    labels[0] = "background"
+    for i, alpha in zip(label_classes, np.linspace(0.5, 1, len(label_classes))):
+        color = ImageColor.getcolor(i["color"], "RGB")
+
+        cmap[i["png_index"]] = [i / 255 for i in color] + [alpha]
+        labels[i["png_index"]] = i["name"]
+
+    cmap[0] = [1, 1, 1, 1]
+
+    # create patches as legend
+    patches = [mpatches.Patch(color=cmap[i], label=labels[i]) for i in cmap]
+
+    return cmap, patches
